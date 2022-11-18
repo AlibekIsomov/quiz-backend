253fcd66473efe694a853b4f37f7265ef4a622c8
Ali <022fozilovuktam@gmail.com>
2022-11-19 00:06:52 +0500

    ok

---
+++ src/main/java/com/quiz/service/witDto/QuestionDTOService.java
@@ -0,0 +1,7 @@
+package com.quiz.service.witDto;
+
+import com.quiz.dto.QuestionDTO;
+import com.quiz.entity.Question;
+
+public interface QuestionDTOService extends CommonServiceDto<Question, QuestionDTO> {
+}
